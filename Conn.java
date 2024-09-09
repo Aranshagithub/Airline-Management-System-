ba3ffe1e13ea09510d6df8eb5523a9f23bbcf551
Aranshagithub <aranshavadhera@gmail.com>
2024-09-09 15:32:53 +0530

    Add files via upload

---
+++ Conn.java
@@ -0,0 +1,22 @@
+package airlinemanagementsystem;
+
+import java.sql.*;
+
+public class Conn {
+    
+    Connection c;
+    Statement s;
+    
+    
+public Conn()  {
+    try{
+        class.forName("com.mysql.cj.jdbc.Driver");
+        c= DriverManager.getConnection("jdbc:mysql:///airlinemanagementsystem","root","1234");
+        s= c.createStatement();
+        
+    }catch(Exception e) {
+        e.printStackTrace();
+        
+     }
+  }
+}
